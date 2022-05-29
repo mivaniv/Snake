fd5aa0237451627e0980eff8d6030967a4d8d855
mivaniv <67320119+mivaniv@users.noreply.github.com>
2022-05-29 19:09:08 +0300

    Add Game

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GamePage from "./components/gamePage/gamePage";
+
+function App() {
+    return (
+        <div className="App">
+            <GamePage/>
+        </div>
+    );
+}
+
+export default App;
